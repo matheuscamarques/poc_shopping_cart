ae6b8149990d5d96b0a975214e7c08c59199d7cb
Matheus C Marques <matheuscamarques@gmail.com>
2023-09-06 13:39:25 -0300

    fix: Cart items Sage complete functions

--- lib/shopping_cart/budget/sage/carts_items_sage.ex
+++ lib/shopping_cart/budget/sage/carts_items_sage.ex
@@ -1,8 +1,7 @@
 defmodule ShoppingCart.Budget.Sage.CartsItemsSage do
   import Sage
-  def create_cart_items(%{}) do
-    {:error, "empty map"}
-  end
+  alias ShoppingCart.Repo
+
   def create_cart_items(params) do
     new()
     |> run(:verify_cart, &verify_cart/2)
@@ -15,31 +14,34 @@ defmodule ShoppingCart.Budget.Sage.CartsItemsSage do
   def verify_cart(_,%{params: %{cart_id: cart_id}}) do
     case ShoppingCart.Budget.get_carts(cart_id) do
       nil -> {:error, "cart not founded, try to create or verify id"}
-      cart -> {:ok , cart}
+      cart ->
+        IO.inspect("founded cart")
+        {:ok , cart}
     end
   end
 
   def verify_item(_,%{params: %{item_id: item_id}}) do
     case ShoppingCart.Budget.get_items(item_id) do
       nil -> {:error, "item not founded, try to create or verify id"}
-      item -> {:ok , item}
+      item ->
+        IO.inspect("founded item")
+        {:ok , item}
     end
   end
 
   def create_cart_item(_,%{params: %{item_id: item_id,cart_id: cart_id}}) do
-    ShoppingCart.Budget.create_cart_items(%{
-      cart_id: cart_id,
-      item_id: item_id
-    })
+    %ShoppingCart.Budget.CartsItems{ cart_id: cart_id,item_id: item_id}
+    |> Repo.insert()
   end
 
   def update_cart(%{verify_cart: cart, verify_item: item},_) do
     changeset = %{
       total_term_price: cart.total_term_price + item.term_price,
       total_items: cart.total_items + 1,
-      total_price: cart.total_price + item.term_price * (item.discount_percentage/100)
+      total_price: round(cart.total_price + (item.term_price - item.term_price * (item.discount_percentage/100)))
     }
 
     ShoppingCart.Budget.update_cart(cart,changeset)
   end
+
 end
