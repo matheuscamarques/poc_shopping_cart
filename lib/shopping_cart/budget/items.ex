4bcb4a280600af781728b090b9082cba8d25b781
Matheus C Marques <matheuscamarques@gmail.com>
2023-09-05 22:03:56 -0300

    add: Items Schema

---
+++ lib/shopping_cart/budget/items.ex
@@ -0,0 +1,22 @@
+defmodule ShoppingCart.Budget.Items do
+  use Ecto.Schema
+  import Ecto.Changeset
+  @primary_key {:id, :binary_id, autogenerate: true}
+      @foreign_key_type :binary_id
+
+  schema "items" do
+    field :desc, :string
+    field :discount_percentage, :integer
+    field :name, :string
+    field :term_price, :integer
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(items, attrs) do
+    items
+    |> cast(attrs, [:name, :desc, :term_price, :discount_percentage])
+    |> validate_required([:name, :desc, :term_price, :discount_percentage])
+  end
+end
