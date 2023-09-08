007cd37f0a6236dc42497b7646ca4b3bb0a96cf4
Matheus C Marques <matheuscamarques@gmail.com>
2023-09-08 10:37:02 -0300

    fix: names in migration

--- priv/repo/migrations/20230905185454_create_cart_items.exs
+++ priv/repo/migrations/20230905185454_create_cart_items.exs
@@ -4,8 +4,8 @@ defmodule ShoppingCart.Repo.Migrations.CreateCartItems do
   def change do
     create table(:carts_items, primary_key: false) do
       add(:id, :binary_id, primary_key: true)
-      add(:cart_id, references(:carts, type: :binary_id))
-      add(:item_id, references(:items, type: :binary_id))
+      add(:carts_id, references(:carts, type: :binary_id))
+      add(:items_id, references(:items, type: :binary_id))
 
       timestamps()
     end
