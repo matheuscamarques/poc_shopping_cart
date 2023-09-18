d3a844a5726f8894f9b1e68f45985cf14ba4ae1e
Matheus C Marques <matheuscamarques@gmail.com>
2023-09-18 13:07:13 -0300

    fix: comment :kaffe configuration

--- config/dev.exs
+++ config/dev.exs
@@ -1,22 +1,22 @@
 import Config
 
-config :kaffe,
-  consumer: [
-    endpoints: [localhost: 9092],
-    consumer_group: "consumer-group",
-    message_handler: MessageProcessor,
-    # endpoints references [hostname: port]. Kafka is configured to run on port 9092.
-    # In this example, the hostname is localhost because we've started the Kafka server
-    # straight from our machine. However, if the server is dockerized, the hostname will
-    # be called whatever is specified by that container (usually "kafka")
-    # add a list of topics you plan to produce messages to
-    topics: ["payments"]
-  ],
-  producer: [
-    # [hostname: port]
-    endpoints: [localhost: 9092],
-    topics: ["payments"]
-  ]
+# config :kaffe,
+#   consumer: [
+#     endpoints: [localhost: 9092],
+#     consumer_group: "consumer-group",
+#     message_handler: MessageProcessor,
+#     # endpoints references [hostname: port]. Kafka is configured to run on port 9092.
+#     # In this example, the hostname is localhost because we've started the Kafka server
+#     # straight from our machine. However, if the server is dockerized, the hostname will
+#     # be called whatever is specified by that container (usually "kafka")
+#     # add a list of topics you plan to produce messages to
+#     topics: ["payments"]
+#   ],
+#   producer: [
+#     # [hostname: port]
+#     endpoints: [localhost: 9092],
+#     topics: ["payments"]
+#   ]
 
 # Configure your database
 config :shopping_cart, ShoppingCart.Repo,
